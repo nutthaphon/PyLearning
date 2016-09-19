d7dc409c8f0e62de7102f847d22f64852f8f416b
nutthaphon <nutthaphon@gmail.com>
2016-09-19 22:59:38 +0700

    First Spider

---
+++ Scraping/MySpider.py
@@ -0,0 +1,21 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+class MySpider(scrapy.Spider):
+    # Your spider definition
+    name = 'blogspider'
+    start_urls = ['https://blog.scrapinghub.com']
+    def parse(self, response):
+        for title in response.css('h2.entry-title'):
+            yield {'title': title.css('a ::text').extract_first()}
+
+        next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
+        if next_page:
+            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(MySpider)
+process.start() # the script will block here until the crawling is finished
---
+++ serial/testJSON.py
@@ -0,0 +1,9 @@
+'''
+Created on Sep 19, 2016
+
+@author: nutt
+'''
+
+import json
+json_string = json.dumps([1, 2, 3, "a", "b", "c"])
+print json.loads(json_string)
