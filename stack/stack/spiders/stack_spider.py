500e58c4492db8f9b40d292a5472e8f53afce6de
nutthaphon <nutthaphon@gmail.com>
2016-12-20 16:41:56 +0700

    OK

--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -8,11 +8,12 @@ class StackSpider(Spider):
     name = "stack"
     allowed_domains = ["stackoverflow.com"]
     start_urls = [
-        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+      #  "http://stackoverflow.com/questions?pagesize=50&sort=newest"
+        "http://pantip.com/forum/mbk"
     ]
 
     def parse(self, response):
-        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+        questions = Selector(response).xpath('//div[@class="post-item-title"]')
         print "zzzzzzzzzzzzzzzzzzz"
         for question in questions:
             item = StackItem()
