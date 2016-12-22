b46e8ddbf7ea2965258be4a88678ee595cca711c
Nutthaphon Suwanwong <nutthaphon@github.com>
2016-12-22 14:36:22 +0700

    deployed files of Scrapyd

---
+++ stack/build/lib.linux-x86_64-2.7/stack/spiders/stack_spider.py
@@ -0,0 +1,28 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from stack.items import StackItem
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+      #  "http://stackoverflow.com/questions?pagesize=50&sort=newest"
+        "http://pantip.com/forum/mbk"
+    ]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="post-item-title"]')
+        
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath('a/text()').extract()[0]
+            item['url'] = question.xpath('a/@href').extract()[0]
+            #print "Topic link = %s" % item['title']
+            #    $x("//div[@class=\"post-item-title\"]")[0].innerText
+            #item['url'] = question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
+
+
+
