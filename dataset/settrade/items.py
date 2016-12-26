323cf8d4afc31e2935b52195cddcc04f25ae66c0
nutthaphon <nutthaphon@gmail.com>
2016-12-26 22:51:39 +0700

    ok

--- dataset/settrade/items.py
+++ dataset/settrade/items.py
@@ -10,6 +10,11 @@ from scrapy.item import Item, Field
 class SettradeItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    title = Field()
-    link = Field()
-    description = Field()
+    UpdateDT = Field()
+    Prior = Field()
+    Last = Field()
+    Chg = Field()
+    Volume = Field()
+    Value = Field()
+    AccTotalVol = Field()
+    AccTotalVal = Field()
